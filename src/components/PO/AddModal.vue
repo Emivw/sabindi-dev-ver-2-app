8da5fc3d7e0962b3d184f3f5ddd9871c5c9dfe10
Emivw <emiya.vanwyk@gmail.com>
2022-11-02 08:07:08 +0200

    push of Purchase order

--- src/components/PO/AddModal.vue
+++ src/components/PO/AddModal.vue
@@ -11,78 +11,22 @@
         <div class="modal-body">
           <form>
             <div class="mb-3">
-              <label for="recipient-name" class="col-form-label">Entry Type</label>
-              <input type="text" class="form-control" id="recipient-name" placeholder="entryType" v-model="entryType" />
+              <label for="recipient-name" class="col-form-label">Qoute ID:</label>
+              <input type="text" class="form-control" id="recipient-name" placeholder="quote ID" v-model="qteID" />
             </div>
             <div class="mb-3">
-              <label for="recipient-name" class="col-form-label">uid:</label>
-              <input type="text" class="form-control" id="recipient-name" placeholder="uid" v-model="uid" />
+              <label for="recipient-name" class="col-form-label">Materials:</label>
+              <input type="text" class="form-control" id="recipient-name" placeholder="materials" v-model="mat" />
             </div>
             <div class="mb-3">
-              <label for="recipient-name" class="col-form-label">Customer Name</label>
-              <input type="text" class="form-control" id="recipient-name" placeholder="Customer Name"
-                v-model="cusName" />
+              <label for="recipient-name" class="col-form-label">One Time Pin</label>
+              <input type="text" class="form-control" id="recipient-name" placeholder="One Time Pin"
+                v-model="otp" />
             </div>
             <div class="mb-3">
-              <label for="recipient-name" class="col-form-label">Customer Number:</label>
-              <input type="text" class="form-control" id="recipient-name" placeholder="Customer Number"
-                v-model="cusNo" />
-            </div>
-            <div class="mb-3">
-              <label for="recipient-name" class="col-form-label">Customer Address:</label>
-              <input type="text" class="form-control" id="recipient-name" placeholder="cusAddress"
-                v-model="cusAddress" />
-            </div>
-            <div class="mb-3">
-              <label for="recipient-name" class="col-form-label">Insurance Category:</label>
-              <input type="text" class="form-control" id="recipient-name" placeholder="insCat" v-model="insCat" />
-            </div>
-            <div class="mb-3">
-              <label for="recipient-name" class="col-form-label">Leak Detection Method:</label>
-              <input type="text" class="form-control" id="recipient-name" placeholder="leakDetectMethod"
-                v-model="leakDetectMethod" />
-            </div>
-            <div class="mb-3">
-              <label for="recipient-name" class="col-form-label">Damage Location:</label>
-              <input type="text" class="form-control" id="recipient-name" placeholder="dmgLocation"
-                v-model="dmgLocation" />
-            </div>
-            <div class="mb-3">
-              <label for="recipient-name" class="col-form-label">Damage Status:</label>
-              <input type="text" class="form-control" id="recipient-name" placeholder="dmgStatus" v-model="dmgStatus" />
-            </div>
-            <div class="mb-3">
-              <label for="recipient-name" class="col-form-label">RepRecom:</label>
-              <input type="text" class="form-control" id="recipient-name" placeholder="RepRecom" v-model="RepRecom" />
-            </div>
-            <div class="mb-3">
-              <label for="recipient-name" class="col-form-label">Quote Description:</label>
-              <input type="text" class="form-control" id="recipient-name" placeholder="qtDesc" v-model="qtDesc" />
-            </div>
-            <div class="mb-3">
-              <label for="recipient-name" class="col-form-label">Summary:</label>
-              <input type="text" class="form-control" id="recipient-name" placeholder="summary" v-model="summary" />
-            </div>
-            <div class="mb-3">
-              <label for="recipient-name" class="col-form-label">Job Category:</label>
-              <input type="text" class="form-control" id="recipient-name" placeholder="jobCat" v-model="jobCat" />
-            </div>
-            <div class="mb-3">
-              <label for="recipient-name" class="col-form-label">Quote Materials:</label>
-              <input type="text" class="form-control" id="recipient-name" placeholder="qteMaterials"
-                v-model="qteMaterials" />
-            </div>
-            <div class="mb-3">
-              <label for="recipient-name" class="col-form-label">Scope:</label>
-              <input type="text" class="form-control" id="recipient-name" placeholder="scope" v-model="scope" />
-            </div>
-            <div class="mb-3">
-              <label for="recipient-name" class="col-form-label">Total:</label>
-              <input type="text" class="form-control" id="recipient-name" placeholder="total" v-model="total" />
-            </div>
-            <div class="mb-3">
-              <label for="recipient-name" class="col-form-label">Add notes:</label>
-              <input type="text" class="form-control" id="recipient-name" placeholder="addNote" v-model="addNote" />
+              <label for="recipient-name" class="col-form-label">Supplier ID:</label>
+              <input type="text" class="form-control" id="recipient-name" placeholder="Supplier ID"
+                v-model="sID" />
             </div>
 
           </form>
@@ -102,52 +46,24 @@
 
 <script>
 export default {
-  props: ['lead'],
+  props: ['po'],
   data() {
     return {
-      entryType: "",
-      uid: "",
-      cusName: "",
-      cusNo: "",
-      cusAddresss: "",
-      damageType: "",
-      insCat: "",
-      leakDetectMethod: "",
-      dmgLocation: "",
-      dmgStatus: "",
-      RepRecom: "",
-      qtDesc: "",
-      summary: "",
-      jobCat: "",
-      qteMaterials: "",
-      scope: "",
-      total: "",
-      addNote: ""
+      qteID: "",
+      mat: "",
+      sID: "",
+      otp: "",
     };
   },
   methods: {
     add() {
       let payload = {
-        entryType: this.entryType,
-        uid: this.uid,
-        cusName: this.cusName,
-        cusNo: this.cusNo,
-        cusAddresss: this.cusAddress,
-        damageType: this.damageType,
-        insCat: this.insCat,
-        leakDetectMethod: this.leakDetectMethod,
-        dmgLocation: this.dmgLocation,
-        dmgStatus: this.dmgStatus,
-        RepRecom: this.RepRecom,
-        qtDesc: this.qtDesc,
-        summary: this.summary,
-        jobCat: this.jobCat,
-        qteMaterials: this.qteMaterials,
-        scope: this.scope,
-        total: this.total,
-        addNote: this.addNote,
+      qteID: this.qteID,
+      mat: this.mat,
+      sID: this.sID,
+      otp: this.otp,
       }
-      this.$store.dispatch("createQuote", payload);
+      this.$store.dispatch("createPO", payload);
     }
   }
 }
